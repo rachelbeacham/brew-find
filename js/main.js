7fb6c2ab0f94071791cf46f42703f72e20ec2434
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-10 19:51:26 +0000

    Declare a variable that holds XMLHTTP request data. #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 var $zipInput = document.querySelector('.zip-input')
-console.log($zipInput)
 
-$zipInput.addEventListener('input', pullData)
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://raw.githubusercontent.com/openbrewerydb/openbrewerydb/master/breweries.json');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log('xhr status:', xhr.status);
+  console.log('xhr response:', xhr.response);
+});
+xhr.send();
 
-function pullData(e) {
+$zipInput.addEventListener('input', renderOptions)
+
+function renderOptions(e) {
 
 }
