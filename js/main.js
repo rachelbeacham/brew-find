365ba3e7c592a1a5331b17bc124fe73de8436510
Rachel Beacham <rachelbeacham17@gmail.com>
2020-11-11 01:03:53 +0000

    Create a 'welcome' data view that shows a welcome message and a form to enter their city. Create a 'brewery-options' data view that shows the list of brewery options in their city. Create a view swapping function that shows the current view and hides all others. #1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,65 @@
-var $zipInput = document.querySelector('.zip-input')
+var $inputForm = document.querySelector('.city-input-form')
+var $cityInput = document.querySelector('.city-input')
+var $optionRow = document.querySelector('.option-row')
+var $dataViews = document.querySelectorAll('.data-view')
+var dataArray = [];
+
+$inputForm.addEventListener('submit', formSubmitted);
+
+function formSubmitted(event) {
+  event.preventDefault();
+  data.location = $cityInput.value
+  $inputForm.reset();
+  viewSwapping('brewery-options');
+}
+
+function viewSwapping(currentValue) {
+   if (currentValue === 'welcome') {
+     $dataViews[0].className = 'data-view';
+     $dataViews[1].className = 'data-view hidden'
+   }
+   if (currentValue === 'brewery-options') {
+     $dataViews[0].className = 'data-view hidden'
+     $dataViews[1].classList = 'data-view'
+     renderOptions();
+   }
+}
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://raw.githubusercontent.com/openbrewerydb/openbrewerydb/master/breweries.json');
+xhr.open('GET', 'https://api.openbrewerydb.org/breweries?by_city=' + data.location);
 xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
-  console.log('xhr status:', xhr.status);
-  console.log('xhr response:', xhr.response);
+  dataArray = xhr.response;
+  console.log('dataArray:', dataArray);
 });
 xhr.send();
 
-$zipInput.addEventListener('input', renderOptions)
+function renderOptions(data) {
+  var $colHalfDiv = document.createElement('div');
+  $colHalfDiv.className = 'col-half';
 
-function renderOptions(e) {
+  var $imageDiv = document.createElement('div');
+  $imageDiv.className = 'image-div';
 
-}
+  var $brewInfoCol = document.createElement('div');
+  $brewInfoCol.className = 'column brew-info text-center';
+
+  var $brewName = document.createElement('p');
+  $brewName.textContent = dataArray[i].name;
+
+  var $brewAddress = document.createElement('p');
+  $brewAddress.textContent = dataArray[i].street + ', ' + dataArray[i].city + ' ' + dataArray[i].state + ' ' + dataArray[i].postal_code
+
+  var $addToFavorites = document.createElement('p');
+  $addToFavorites.textContent = 'Add to favorites';
+  $addToFavorites.className = 'add-to-favorites-link';
+
+
+  $colHalfDiv.appendChild($imageDiv);
+  $imageDiv.appendChild($brewInfoCol);
+  $brewInfoCol.appendChild($brewName);
+  $brewInfoCol.appendChild($brewAddress);
+  $brewInfoCol.appendChild($addToFavorites);
+
+  return $colHalfDiv;
+  }
